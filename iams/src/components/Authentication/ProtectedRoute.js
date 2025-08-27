ad5b6f08f8e9f4ff4d2750509a4f127b1225d661
Bùi Hằng <101080795+buihang002@users.noreply.github.com>
2025-08-27 12:39:32 +0700

    change

--- iams/src/components/Authentication/ProtectedRoute.js
+++ iams/src/components/Authentication/ProtectedRoute.js
@@ -1,35 +1,3 @@
-// import React from "react";
-// import { Navigate, Outlet } from "react-router-dom";
-// // import MainLayout from "../MainLayout/MainLayout";
-// // const ProtectedRoute = () => {
-// //   const user = JSON.parse(localStorage.getItem("user"));
-
-// //   if (!user) {
-// //     return <Navigate to="/login" replace />;
-// //   }
-
-// //   // if (loggedInUser && allowedRoles.includes(loggedInUser.role)) {
-// //   //   return <MainLayout />;
-// //   // } else {
-// //   //   return <Navigate to="/login" />;
-// //   // }
-// // };
-
-// // export default ProtectedRoute;
-// const ProtectedRoute = ({ children, allowedRoles }) => {
-//   const user = JSON.parse(localStorage.getItem("user"));
-
-//   if (!user) {
-//     return <Navigate to="/login" replace />;
-//   }
-
-//   return allowedRoles.includes(user.role) ? (
-//     children
-//   ) : (
-//     <Navigate to="/" replace />
-//   );
-// };
-// export default ProtectedRoute;
 import React from "react";
 import { Navigate } from "react-router-dom";
 
